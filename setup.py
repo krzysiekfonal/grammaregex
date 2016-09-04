16064a68c399aec59f7524216b7a1f00c0670203
Krzysiek <krzysiekfonal@gmail.com>
2016-09-04 17:26:14 +0200

    Updated release version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='grammaregex',
-      version='0.1',
+      version='0.1.1',
       description='grammaregex - library for matching and finding tree sentence in regex-like way',
       long_description='This library allow you to find single tokens in sentences or match sentence by grammar regex-like expressions.',
       classifiers=[
